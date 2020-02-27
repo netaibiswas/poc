2bb0f8563115daeb5052e900be3b542ea8baa391
Netai Biswas <reachnetai@gmail.com>
2020-02-27 11:17:16 +0530

    Test

--- src/main/java/com/nb/poc/demo/DemoApplication.java
+++ src/main/java/com/nb/poc/demo/DemoApplication.java
@@ -10,9 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	/**
+	 * @param args
+	 */
+	public static void main(final String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-
 	}
 
 }
--- src/test/java/com/nb/poc/demo/DemoApplicationTests.java
+++ src/test/java/com/nb/poc/demo/DemoApplicationTests.java
@@ -1,19 +1,38 @@
 package com.nb.poc.demo;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
+/**
+ * @author netaipada.biswas
+ *
+ */
 @SpringBootTest
 class DemoApplicationTests {
 
 	/**
 	 * Dummy
+	 * @throws URISyntaxException
 	 */
 	@Test
-	void contextLoads() {
-		assertTrue(true);
+	void contextLoads() throws URISyntaxException {
+		RestTemplate restTemplate = new RestTemplate();
+
+		final String baseUrl = "http://localhost:8080";
+		URI uri = new URI(baseUrl);
+
+		ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+
+		// Verify request succeed
+		assertEquals(200, result.getStatusCodeValue());
+		assertEquals(true, result.getBody().contains("employeeList"));
 	}
 
 }
