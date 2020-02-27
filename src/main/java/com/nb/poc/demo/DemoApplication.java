80d7249ecb58994e8f541c839577195c95227fd4
Netai Biswas <reachnetai@gmail.com>
2020-02-27 11:04:14 +0530

    Test

--- src/main/java/com/nb/poc/demo/DemoApplication.java
+++ src/main/java/com/nb/poc/demo/DemoApplication.java
@@ -1,14 +1,18 @@
 package com.nb.poc.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author netaipada.biswas
+ *
+ */
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+		SpringApplication.run(DemoApplication.class, args);
+
 	}
 
 }
