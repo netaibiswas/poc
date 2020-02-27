1d646d81d3f2e3d431cc1fdb8f42428e069ab11a
Netai Biswas <reachnetai@gmail.com>
2020-02-27 11:07:50 +0530

    Test

--- src/test/java/com/nb/poc/demo/DemoApplicationTests.java
+++ src/test/java/com/nb/poc/demo/DemoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.nb.poc.demo;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	/**
+	 * Dummy
+	 */
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
